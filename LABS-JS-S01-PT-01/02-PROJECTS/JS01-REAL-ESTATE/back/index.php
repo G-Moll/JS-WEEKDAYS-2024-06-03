fa22464748b2a6eb007876f2ee79c4884616104c
RDK <photocromatic@gmail.com>
2024-06-26 13:09:30 -0600

    LABs

--- LABS-JS-S01-PT-01/02-PROJECTS/JS01-REAL-ESTATE/back/index.php
+++ LABS-JS-S01-PT-01/02-PROJECTS/JS01-REAL-ESTATE/back/index.php
@@ -7,32 +7,21 @@ $db_pass = "";
 $db_name = "rdk_js_weekdays";
 
 $conn = mysqli_connect( $db_host, $db_user, $db_pass, $db_name );
-if( ! $conn ) {
-	die( "DB connection failed.."  );
-}
+
+if( ! $conn ) { die( "DB connection failed.."  ); }
 
 $query = "SELECT * FROM draftproperties";
 $query_result = mysqli_query( $conn, $query );
+$all_records = [];
+
+while( $record = mysqli_fetch_assoc( $query_result ) ) {
+	array_push( $all_records, $record );
+}
 
 $json_data = [
-	"data" => mysqli_fetch_assoc( $query_result )
+	"data" => $all_records
 ];
 
 echo json_encode( $json_data );
-
-
-// while( $record = ) ) {
-// 	echo "property" . $record[ "property" ];
-// }
-
-// $json_data = [
-// 	"data" => [
-// 		[ "property" => "house", "location" => true, "price" => 30000 ],
-// 		[ "property" => "apartment", "location" => true, "price" => 40000 ],
-// 		[ "property" => "house", "location" => false, "price" => 50000 ]
-// 	]
-// ];
-
-// echo json_encode( $json_data );
 
 ?>

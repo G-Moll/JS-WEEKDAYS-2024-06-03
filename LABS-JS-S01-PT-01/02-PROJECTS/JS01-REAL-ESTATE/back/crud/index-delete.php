e121939e8f25680924bd95ad427eee6080c6b68c
RDK <photocromatic@gmail.com>
2024-07-09 12:23:48 -0600

    LABs

--- LABS-JS-S01-PT-01/02-PROJECTS/JS01-REAL-ESTATE/back/crud/index-delete.php
+++ LABS-JS-S01-PT-01/02-PROJECTS/JS01-REAL-ESTATE/back/crud/index-delete.php
@@ -6,16 +6,16 @@ $db_user = "root";
 $db_pass = "";
 $db_name = "rdk_js_weekdays";
 
-$property = $_POST[ 'property' ];
-$location = $_POST[ 'location' ];
-$price    = $_POST[ 'price' ];
+$id = $_POST[ 'id' ];
 
 $conn = mysqli_connect( $db_host, $db_user, $db_pass, $db_name );
 if( ! $conn ) { die( "DB connection failed.."  ); }
 
-$query = "INSERT INTO draftproperties VALUES( '$property', $location, $price )";
+$query = "UPDATE draftproperties SET available=FALSE WHERE id=$id";
+// $query = "DELETE FROM draftproperties WHERE id=$id";
+
 $query_result = mysqli_query( $conn, $query );
-$message = $query_result ? [ "message" => "OK" ] : [ "message" => "NO" ];
+$message = $query_result ? [ "message" => "OK, deleted record" ] : [ "message" => "NO, record could not be deleted" ];
 
 $json_data = [
     "data" => $message
---
+++ LABS-JS-S01-PT-01/02-PROJECTS/JS01-REAL-ESTATE/db/crud/db-delete.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE rdk_js_weekdays;
+USE rdk_js_weekdays;
+
+create table draftproperties(
+	id INT( 11 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	available BOOLEAN NOT NULL DEFAULT TRUE,
+	property VARCHAR( 255 ) NOT NULL,
+	location BOOLEAN NOT NULL,
+	price INT( 11 ) NOT NULL
+);
+
+INSERT INTO draftproperties VALUES( NULL, DEFAULT, "house", true, 30000 );
+INSERT INTO draftproperties VALUES( NULL, DEFAULT, "apartment", true, 30000 );
+INSERT INTO draftproperties VALUES( NULL, DEFAULT, "house", false, 50000 );
+INSERT INTO draftproperties VALUES( NULL, DEFAULT, "parking lot", true ,80000 );
+INSERT INTO draftproperties VALUES( NULL, DEFAULT, "apartment", false, 35000 );
+
+
+SELECT * FROM draftproperties;
+-- DELETE FROM draftproperties WHERE id = 22; HARD DELETE
+-- UPDATE draftproperties SET available=FALSE WHERE id=21; SOFT DELETE
+SELECT * FROM draftproperties WHERE available = TRUE;
+
+
+SHOW DATABASES;
+SHOW TABLES;
+-- DESCRIBE draftproperties;
+-- DROP TABLE draftproperties;
+-- DROP TABLE draftproperties;

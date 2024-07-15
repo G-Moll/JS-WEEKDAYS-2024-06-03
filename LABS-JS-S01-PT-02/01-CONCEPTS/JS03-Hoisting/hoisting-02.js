44dc3344a3cade4984fd04d9e384641aa61cb59e
RDK <photocromatic@gmail.com>
2024-07-15 12:49:36 -0600

    LABs

--- LABS-JS-S01-PT-02/01-CONCEPTS/JS03-Hoisting/hoisting-02.js
+++
@@ -1,6 +0,0 @@
-// Hoisting - ES6#
-// console.log( y );
-let y = 10;
-
-// console.log( x );
-const x = 20;
--- LABS-JS-S01-PT-02/01-CONCEPTS/JS04-Arguments/arguments-01.js
+++
@@ -1,7 +0,0 @@
-// arguments
-
-function a() {
-	console.log( arguments );
-	console.log( arguments[ 0 ] > arguments[ 2 ] );
-}
-a( 10, "F", true );
